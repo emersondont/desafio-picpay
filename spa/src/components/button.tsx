94dc280efdf5be8762019d3ca87266e47b253da7
Emerson Vieira <emersondont@gmail.com>
2024-10-01 01:17:00 -0300

    refactor: Adicionar propriedade 'disabled' ao botão

--- spa/src/components/button.tsx
+++ spa/src/components/button.tsx
@@ -8,7 +8,8 @@ export default function Button(props: ButtonProps) {
 
   return (
     <button
-      className="w-full bg-primary text-white text-lg font-bold p-2 rounded-md hover:bg-primaryHover duration-200 ease-out flex gap-2 justify-center items-center"
+      disabled={props.loading}
+      className="w-full bg-primary text-white text-lg font-bold p-2 rounded-md hover:bg-primaryHover duration-200 ease-out flex gap-2 justify-center items-center disabled:hover:bg-primary disabled:opacity-50"
       {...props}
     >
       <span>{props.children}</span>
